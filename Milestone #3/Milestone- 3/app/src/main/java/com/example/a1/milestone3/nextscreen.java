06a57bd9e89a221b77d8aafdfcb46e8132667bee
Kavss23 <85955426+Kavss23@users.noreply.github.com>
2021-08-12 23:37:22 +0530

    Add files via upload

---
+++ Milestone #3/Milestone- 3/app/src/main/java/com/example/a1/milestone3/nextscreen.java
@@ -0,0 +1,14 @@
+package com.example.a1.milestone3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class nextscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nextscreen);
+    }
+}
