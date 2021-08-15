3a3a1bf9091d1e4150a46f5a68f7eda569a0b3e0
Kavss23 <85955426+Kavss23@users.noreply.github.com>
2021-08-15 23:37:41 +0530

    Add files via upload

---
+++ Milestone #3/Milestone 3/app/src/main/java/com/example/a1/milestone3/Activity2.java
@@ -0,0 +1,46 @@
+package com.example.a1.milestone3;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class Activity2 extends AppCompatActivity {
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nextscreen);
+
+
+        View cam = findViewById(R.id.camera);
+
+
+        cam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent();
+                    intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivity(intent);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+          
+
+
+
+
+
+    }
+}
+
